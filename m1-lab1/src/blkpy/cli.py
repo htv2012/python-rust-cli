0564b0f4baddb8645f8f8b098354efe502b294f1
Hai Vu <haivu2004@gmail.com>
2024-12-10 00:06:49 -0800

    Refactor to simplify code logic

--- m1-lab1/src/blkpy/cli.py
+++ m1-lab1/src/blkpy/cli.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import json
-
 import click
 import rich
 
@@ -11,14 +9,12 @@ from .util import find_first, run_lsblk
 @click.argument("device")
 def main(device):
     data = run_lsblk()["blockdevices"]
-    if device == "all":
-        device_info = data
-    else:
+    if device != "all":
         try:
-            device_info = find_first(data, device)
+            data = find_first(data, device)
         except ValueError as error:
             raise SystemExit(str(error))
-    rich.print_json(json.dumps(device_info, indent=4))
+    rich.print_json(data=data, indent=4)
 
 
 if __name__ == "__main__":
