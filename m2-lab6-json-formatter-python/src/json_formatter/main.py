ef4e3e39e68e0cc9ed25286ee8ced8d06d8e0fe1
Hai Vu <haivu2004@gmail.com>
2024-12-25 08:36:59 -0800

    Rename option --file-name to --filename

--- m2-lab6-json-formatter-python/src/json_formatter/main.py
+++ m2-lab6-json-formatter-python/src/json_formatter/main.py
@@ -5,10 +5,10 @@ import rich
 
 
 @click.command()
-@click.option("-f", "--file-name", help="Specify input file")
+@click.option("-f", "--filename", help="Specify input file")
 @click.option("-i", "--indent", type=int, default=2, help="Specify indentation")
 @click.option("-s", "--sort-keys", is_flag=True, help="Sort keys alphabetically")
-def main(file_name, indent, sort_keys):
-    with fileinput.input(file_name) as lines:
+def main(filename, indent, sort_keys):
+    with fileinput.input(filename) as lines:
         json_text = "".join(lines)
     rich.print_json(json_text, indent=indent, sort_keys=sort_keys)
