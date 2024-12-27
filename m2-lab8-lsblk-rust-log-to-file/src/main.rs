ac8261567c6e911c8dd42a12d1c842c503320ce5
Hai Vu <haivu2004@gmail.com>
2024-12-26 23:55:02 -0800

    Add m2-lab8-lsblk-rust-log-to-file

---
+++ m2-lab8-lsblk-rust-log-to-file/src/main.rs
@@ -0,0 +1,48 @@
+// main.rs
+mod arguments;
+mod device;
+use env_logger::{Builder, Target};
+use log::LevelFilter;
+use std::fs::OpenOptions;
+
+fn main() {
+    let parsed = arguments::parse();
+
+    let mut builder = Builder::new();
+    builder.filter_level(if parsed.debug {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    });
+
+    if parsed.log_to_file {
+        let file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("blkrs.log")
+            .unwrap();
+        builder.target(Target::Pipe(Box::new(file)));
+    }
+
+    builder.init();
+
+    log::debug!("debug = {}", parsed.debug);
+    log::debug!("verbosity level = {}", parsed.verbosity);
+
+    match &parsed.command {
+        arguments::Commands::Info { device } => match device::run_lsblk(device) {
+            Ok(json_value) => match serde_json::to_string_pretty(&json_value) {
+                Ok(json_string) => {
+                    println!("{}", json_string);
+                }
+                Err(msg) => {
+                    println!("{}", json_value);
+                    eprintln!("{}", msg);
+                }
+            },
+            Err(msg) => {
+                eprintln!("{}", msg);
+            }
+        },
+    }
+}
