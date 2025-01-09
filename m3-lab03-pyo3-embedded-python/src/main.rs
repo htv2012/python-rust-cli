8823e0bc391754adea30b0592277b3ff1cb0f6d5
Hai Vu <haivu2004@gmail.com>
2025-01-09 13:33:01 -0800

    Fall back to PyO3 0.19, which works

--- m3-lab03-pyo3-embedded-python/src/main.rs
+++ m3-lab03-pyo3-embedded-python/src/main.rs
@@ -1,26 +1,21 @@
 use pyo3::types::PyString;
-use pyo3::types::PyModule;
-use pyo3::prelude::*;
-
-
-
+use pyo3::{prelude::*, types::PyModule};
 
 fn marco_python(input: &str) -> PyResult<String> {
     pyo3::prepare_freethreaded_python();
-
     Python::with_gil(|py| {
         let marco = PyModule::from_code(
             py,
-            cr#"
-def marco(value):
-    if value == "marco":
-        return "head"
-    return "tail"
+            r#"
+def marco(input):
+    if input == "marco":
+        return "python"
+    else:
+        return "no python"
 "#,
-            cr#"marco.py"#,
-            cr#"marco"#,
-)?;
-
+            "marco.py",
+            "marco",
+        )?;
         let marco_func = marco.getattr("marco")?;
         let marco_result = marco_func.call1((input,))?;
         let marco_result: &PyString = marco_result.extract()?;
@@ -29,6 +24,6 @@ def marco(value):
 }
 
 fn main() {
-    println!("marco => {}", marco_python("marco").unwrap());
-    println!("polo => {}", marco_python("polo").unwrap());
+    println!("From embedded Python: {}", marco_python("marco").unwrap());
+    println!("From embedded Python: {}", marco_python("polo").unwrap());
 }
