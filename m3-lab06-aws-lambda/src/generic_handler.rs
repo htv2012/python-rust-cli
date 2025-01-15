4421dd2d272c8e81df615114013c3ae16bb4f9b1
Hai Vu <haivu2004@gmail.com>
2025-01-14 20:11:15 -0800

    Add m3-lab06-aws-lambda (#12)
    * Add m3-lab06-aws-lambda
    
    * Fix: compile error
    
    * Remove bruno, not needed for now

---
+++ m3-lab06-aws-lambda/src/generic_handler.rs
@@ -0,0 +1,54 @@
+use lambda_runtime::{Error, LambdaEvent};
+use rust_decimal::prelude::*;
+use rust_decimal::Decimal;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub(crate) struct IncomingMessage {
+    total: String,
+    tip: String,
+}
+
+#[derive(Serialize)]
+pub(crate) struct OutgoingMessage {
+    req_id: String,
+    tip_amount: String,
+    total_plus_tip: String,
+}
+
+pub(crate) async fn function_handler(
+    event: LambdaEvent<IncomingMessage>,
+) -> Result<OutgoingMessage, Error> {
+    let total = Decimal::from_str(&event.payload.total).unwrap();
+    let tip_percent = Decimal::from_str(&event.payload.tip).unwrap();
+
+    let tip_amount = (total * tip_percent).round_dp(2);
+    let total_plus_tip = (total + tip_amount).round_dp(2);
+
+    let resp = OutgoingMessage {
+        req_id: event.context.request_id,
+        tip_amount: tip_amount.to_string(),
+        total_plus_tip: total_plus_tip.to_string(),
+    };
+
+    Ok(resp)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lambda_runtime::{Context, LambdaEvent};
+
+    #[tokio::test]
+    async fn test_generic_handler() {
+        let event = LambdaEvent::new(
+            IncomingMessage {
+                total: "100.0".to_string(),
+                tip: "0.18".to_string(),
+            },
+            Context::default(),
+        );
+        let response = function_handler(event).await.unwrap();
+        assert_eq!(response.total_plus_tip, "118.00");
+    }
+}
